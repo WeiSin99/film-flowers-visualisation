7ef6c1025c37aff52200b9d37f18c66685635df9
WeiSin99 <looweisin008@gmail.com>
2022-07-07 15:32:34 +0100

    Select existing petal and bind movie data

--- src/script/main.js
+++ src/script/main.js
@@ -1,5 +1,10 @@
 import * as d3 from 'd3';
-import { movies, pathObj } from './movies';
+import { movies, colorObj } from './movies';
 
-const svg = d3.select('svg');
-console.log(svg);
+d3.select('svg')
+  .selectAll('path')
+  .data(movies)
+  .attr('fill', (d) => colorObj[d.genres[0]] ?? colorObj.Other)
+  .attr('stroke', (d) => colorObj[d.genres[0]] ?? colorObj.Other)
+  .attr('fill-opacity', '0.5')
+  .attr('stroke-width', '2');
