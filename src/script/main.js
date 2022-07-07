1366e4c4789da24018427b061fc06af232764ee1
WeiSin99 <looweisin008@gmail.com>
2022-07-08 00:04:07 +0100

    group element

--- src/script/main.js
+++ src/script/main.js
@@ -1,4 +1,5 @@
 import * as d3 from 'd3';
+import lodash from 'lodash';
 import { movies, topGenres, petalColors, colorObj, petalPaths } from './data';
 import { calculateGridPos } from './utils';
 
@@ -11,7 +12,7 @@ const colorScale = d3
 const pathScale = d3.scaleOrdinal().range(petalPaths);
 
 const minMaxRating = d3.extent(movies, (d) => d.rating);
-const sizeScale = d3.scaleLinear().domain(minMaxRating).range([0.2, 0.75]);
+const sizeScale = d3.scaleLinear().domain(minMaxRating).range([0.2, 0.6]);
 
 const minMaxVoting = d3.extent(movies, (d) => d.votes);
 const numPetalScale = d3
@@ -30,13 +31,32 @@ const flowers = movies.map((movie, i) => {
   };
 });
 
-d3.select('svg')
+const g = d3.select('svg').selectAll('g').data(flowers).enter().append('g');
+
+const path = g
   .selectAll('path')
-  .data(flowers)
+  .data((d) => {
+    return Array.from({ length: d.numPetals }).map((_, i) => {
+      return Object.assign({}, d, { rotate: i * (360 / d.numPetals) });
+    });
+  })
   .enter()
-  .append('path')
+  .append('path');
+
+const text = g.append('text');
+
+g.attr('transform', (d) => `translate(${d.translate})`);
+
+text
+  .attr('text-anchor', 'middle')
+  .attr('dy', '.35em')
+  .style('font-size', '.7em')
+  .style('font-style', 'italic')
+  .text((d) => lodash.truncate(d.title, { length: 20 }));
+
+path
   .attr('d', (d) => d.path)
-  .attr('transform', (d) => `translate(${d.translate})scale(${d.scale})`)
+  .attr('transform', (d) => `rotate(${d.rotate})scale(${d.scale})`)
   .attr('fill', (d) => d.color)
   .attr('stroke', (d) => d.color)
   .attr('fill-opacity', '0.5')
