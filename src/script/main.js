98fe7e2a825fd8e38dc646cc19c2e04f0b314122
WeiSin99 <looweisin008@gmail.com>
2022-07-07 15:19:34 +0100

    initial commit

---
+++ src/script/main.js
@@ -0,0 +1,5 @@
+import * as d3 from 'd3';
+import { movies, pathObj } from './movies';
+
+const svg = d3.select('svg');
+console.log(svg);
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './src/script/main.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src', 'index.html'),
+    }),
+  ],
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
+};
