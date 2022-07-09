bd0ffd51787e72df8092b68fb2a5bda872d12ffd
WeiSin99 <looweisin008@gmail.com>
2022-07-09 13:36:45 +0100

    d3 force

--- src/script/main.js
+++ src/script/main.js
@@ -1,150 +1,116 @@
 import * as d3 from 'd3';
 import lodash from 'lodash';
-import { calculateGridPos } from './utils';
-import { movies, topGenres, petalColors, colorObj, petalPaths } from './data';
+import { calculateData, calculateGraph } from './utils';
+import { movies, topGenres } from './data';
 
-const calculateData = (filteredMovies) => {
-  const colorScale = d3
-    .scaleOrdinal()
-    .domain(topGenres)
-    .range(petalColors)
-    .unknown(colorObj.Other);
+const simulate = (graph) => {
+  const link = d3
+    .select('svg')
+    .selectAll('.link')
+    .data(graph.links, (d) => d.id)
+    .join('line')
+    .classed('link', true)
+    .attr('stroke', '#ccc')
+    .attr('opacity', 0.5);
 
-  const pathScale = d3
-    .scaleOrdinal()
-    .domain(['G', 'PG', 'PG-13', 'R'])
-    .range(petalPaths);
+  const flower = d3
+    .select('svg')
+    .selectAll('g')
+    .data(graph.nodes, (d) => d.title)
+    .join((enter) => {
+      const g = enter.append('g');
 
-  const minMaxRating = d3.extent(movies, (d) => d.rating);
-  const sizeScale = d3.scaleLinear().domain(minMaxRating).range([0.2, 0.6]);
+      g.selectAll('path')
+        .data((d) => {
+          return Array.from({ length: d.numPetals }).map((_, i) => {
+            return { rotate: i * (360 / d.numPetals), ...d };
+          });
+        })
+        .enter()
+        .append('path')
+        .attr('d', (d) => d.path)
+        .attr('transform', (d) => `rotate(${d.rotate})scale(${d.scale})`)
+        .attr('fill', (d) => d.color)
+        .attr('stroke', (d) => d.color)
+        .attr('fill-opacity', '0.5')
+        .attr('stroke-width', '2');
 
-  const minMaxVoting = d3.extent(movies, (d) => d.votes);
-  const numPetalScale = d3
-    .scaleQuantize()
-    .domain(minMaxVoting)
-    .range([5, 6, 7, 8, 9, 10]);
+      g.append('text')
+        .attr('text-anchor', 'middle')
+        .attr('dy', '.35em')
+        .style('font-size', '.7em')
+        .style('font-style', 'italic')
+        .text((d) => lodash.truncate(d.title, { length: 20 }));
 
-  return filteredMovies.map((movie, i) => {
-    return {
-      color: colorScale(movie.genres[0]),
-      path: pathScale(movie.rated),
-      scale: sizeScale(movie.rating),
-      numPetals: numPetalScale(movie.votes),
-      title: movie.title,
-      translate: calculateGridPos(i),
-    };
-  });
+      return g;
+    });
+
+  const genres = d3
+    .select('svg')
+    .selectAll('.genre')
+    .data(graph.genres, (d) => d.label)
+    .join('text')
+    .classed('genre', true)
+    .text((d) => d.label)
+    .attr('text-anchor', 'middle');
+
+  const nodes = graph.nodes.concat(graph.genres);
+  d3.forceSimulation(nodes)
+    .force('link', d3.forceLink(graph.links))
+    .force(
+      'collide',
+      d3.forceCollide((d) => d.scale * 100)
+    )
+    .force('center', d3.forceCenter(540, 930))
+    .on('tick', () => {
+      flower.attr('transform', (d) => `translate(${d.x}, ${d.y})`);
+      genres.attr('transform', (d) => `translate(${d.x}, ${d.y})`);
+      link
+        .attr('x1', (d) => d.source.x)
+        .attr('y1', (d) => d.source.y)
+        .attr('x2', (d) => d.target.x)
+        .attr('y2', (d) => d.target.y);
+    });
 };
 
 const init = () => {
   const flowers = calculateData(movies);
-
-  const g = d3.select('svg').selectAll('g').data(flowers).enter().append('g');
-
-  const path = g
-    .selectAll('path')
-    .data((d) => {
-      return Array.from({ length: d.numPetals }).map((_, i) => {
-        return Object.assign({}, d, { rotate: i * (360 / d.numPetals) });
-      });
-    })
-    .enter()
-    .append('path');
-
-  const text = g.append('text');
-
-  g.attr('transform', (d) => `translate(${d.translate})`);
-
-  text
-    .attr('text-anchor', 'middle')
-    .attr('dy', '.35em')
-    .style('font-size', '.7em')
-    .style('font-style', 'italic')
-    .text((d) => lodash.truncate(d.title, { length: 20 }));
-
-  path
-    .attr('d', (d) => d.path)
-    .attr('transform', (d) => `rotate(${d.rotate})scale(${d.scale})`)
-    .attr('fill', (d) => d.color)
-    .attr('stroke', (d) => d.color)
-    .attr('fill-opacity', '0.5')
-    .attr('stroke-width', '2');
+  const graph = calculateGraph(movies, flowers);
+  simulate(graph);
 };
 init();
 
 document.querySelector('.container').addEventListener('change', function (e) {
-  if (e.target.tagName === 'INPUT') {
-    // e.preventDefault();
-    const filteredGenres = Array.from(
-      document.querySelector('.genre-filter').querySelectorAll('input')
-    ).reduce((arr, input) => {
-      if (input.checked) {
-        arr.push(input.value);
-      }
-      return arr;
-    }, []);
+  if (e.target.tagName !== 'INPUT') return;
 
-    const filteredPG = Array.from(
-      document.querySelector('.pg-filter').querySelectorAll('input')
-    ).reduce((arr, input) => {
-      if (input.checked) {
-        arr.push(input.value);
-      }
-      return arr;
-    }, []);
+  const filteredGenres = Array.from(
+    document.querySelector('.genre-filter').querySelectorAll('input')
+  ).reduce((arr, input) => {
+    if (input.checked) {
+      arr.push(input.value);
+    }
+    return arr;
+  }, []);
 
-    const filteredMovies = movies.filter((movie) => {
-      return (
-        (filteredGenres.includes(movie.genres[0]) ||
-          (filteredGenres.includes('Other') &&
-            !topGenres.includes(movie.genres[0]))) &&
-        filteredPG.includes(movie.rated)
-      );
-    });
-    const filteredFlowers = calculateData(filteredMovies);
-    const t = d3.select('svg').transition().duration(1000);
+  const filteredPG = Array.from(
+    document.querySelector('.pg-filter').querySelectorAll('input')
+  ).reduce((arr, input) => {
+    if (input.checked) {
+      arr.push(input.value);
+    }
+    return arr;
+  }, []);
 
-    d3.select('svg')
-      .selectAll('g')
-      .data(filteredFlowers, (d) => d.title)
-      .join(
-        (enter) => {
-          const g = enter
-            .append('g')
-            .attr('opacity', 0)
-            .attr('transform', (d) => `translate(${d.translate})`);
+  const filteredMovies = movies.filter((movie) => {
+    return (
+      (filteredGenres.includes(movie.genres[0]) ||
+        (filteredGenres.includes('Other') &&
+          !topGenres.includes(movie.genres[0]))) &&
+      filteredPG.includes(movie.rated)
+    );
+  });
+  const filteredFlowers = calculateData(filteredMovies);
 
-          g.selectAll('path')
-            .data((d) => {
-              return Array.from({ length: d.numPetals }).map((_, i) => {
-                return { rotate: i * (360 / d.numPetals), ...d };
-              });
-            })
-            .enter()
-            .append('path')
-            .attr('d', (d) => d.path)
-            .attr('transform', (d) => `rotate(${d.rotate})scale(${d.scale})`)
-            .attr('fill', (d) => d.color)
-            .attr('stroke', (d) => d.color)
-            .attr('fill-opacity', '0.5')
-            .attr('stroke-width', '2');
-
-          g.append('text')
-            .attr('text-anchor', 'middle')
-            .attr('dy', '.35em')
-            .style('font-size', '.7em')
-            .style('font-style', 'italic')
-            .text((d) => lodash.truncate(d.title, { length: 20 }));
-
-          return g;
-        },
-        (update) => update,
-        (exit) => {
-          exit.transition(t).attr('opacity', 0).remove();
-        }
-      )
-      .transition(t)
-      .attr('opacity', 1)
-      .attr('transform', (d) => `translate(${d.translate})`);
-  }
+  const filteredGraph = calculateGraph(filteredMovies, filteredFlowers);
+  simulate(filteredGraph);
 });
--- src/script/utils.js
+++ src/script/utils.js
@@ -1,3 +1,6 @@
+import * as d3 from 'd3';
+import { movies, topGenres, petalColors, colorObj, petalPaths } from './data';
+
 const calculateGridPos = (i) => {
   const width = 1152;
   const pathWidth = 120;
@@ -9,4 +12,64 @@ const calculateGridPos = (i) => {
   ];
 };
 
-export { calculateGridPos };
+const calculateData = (filteredMovies) => {
+  const colorScale = d3
+    .scaleOrdinal()
+    .domain(topGenres)
+    .range(petalColors)
+    .unknown(colorObj.Other);
+
+  const pathScale = d3
+    .scaleOrdinal()
+    .domain(['G', 'PG', 'PG-13', 'R'])
+    .range(petalPaths);
+
+  const minMaxRating = d3.extent(movies, (d) => d.rating);
+  const sizeScale = d3.scaleLinear().domain(minMaxRating).range([0.2, 0.6]);
+
+  const minMaxVoting = d3.extent(movies, (d) => d.votes);
+  const numPetalScale = d3
+    .scaleQuantize()
+    .domain(minMaxVoting)
+    .range([5, 6, 7, 8, 9, 10]);
+
+  return filteredMovies.map((movie, i) => {
+    return {
+      color: colorScale(movie.genres[0]),
+      path: pathScale(movie.rated),
+      scale: sizeScale(movie.rating),
+      numPetals: numPetalScale(movie.votes),
+      title: movie.title,
+      translate: calculateGridPos(i),
+    };
+  });
+};
+
+const calculateGraph = (movies, flowers) => {
+  const genres = {};
+  const nodes = [];
+  const links = [];
+
+  movies.forEach((movie, i) => {
+    nodes.push(flowers[i]);
+
+    movie.genres.forEach((genre) => {
+      if (!genres[genre]) {
+        genres[genre] = {
+          label: genre,
+          size: 0,
+        };
+      }
+      genres[genre].size += 1;
+
+      links.push({
+        source: genres[genre],
+        target: flowers[i],
+        id: `${genre}-movie${i}`,
+      });
+    });
+  });
+
+  return { nodes, genres: Object.values(genres), links };
+};
+export { calculateGridPos, calculateData, calculateGraph };
